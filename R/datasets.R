e45bfe34ec56bc0acb11aaacc3e14ee800d1ff12
Felix Mannhardt <felix.mannhardt@sintef.no>
2020-01-15 11:33:04 +0100

    Spelling mistakes

--- R/datasets.R
+++ R/datasets.R
@@ -1,6 +1,6 @@
 #' @name L_heur_1
 #' @title Heuristics miner example log #1
-#' @description Artifical eventlog for illustrating Heuristics Miner published as supplementary material to the book
+#' @description Artificial eventlog for illustrating Heuristics Miner published as supplementary material to the book
 #'  `Process Mining: Discovery, Conformance and Enhancement of Business Processes`.
 #' @docType data
 #' @format Eventlog containing 40  cases
@@ -11,7 +11,7 @@
 
 #' @name L_heur_2
 #' @title Heuristics miner example log #2
-#' @description Artifical eventlog for illustrating Heuristics Miner published as supplementary material to the book
+#' @description Artificial eventlog for illustrating Heuristics Miner published as supplementary material to the book
 #'  `Process Mining: Discovery, Conformance and Enhancement of Business Processes`.
 #' @docType data
 #' @format Eventlog containing 85 cases
@@ -22,9 +22,9 @@
 
 #' @name hospital_multi_perspective
 #' @title Hospital example event log capturing multi-perspectives
-#' @description Sample of 10000 traces from an artifical eventlog from the PhD thesis 'Multi-perspective Process Mining' used to illustrate the Data-aware Heuristic Miner algorithm.
+#' @description Sample of 10 000 traces from an artificial eventlog from the PhD thesis 'Multi-perspective Process Mining' used to illustrate the Data-aware Heuristic Miner algorithm.
 #' @docType data
-#' @format Eventlog containing a sample of 10000 cases
+#' @format Eventlog containing a sample of 10 000 cases
 #' @keywords datasets
 #' @references Mannhardt, F. (Felix) (2016) Data-driven Process Discovery - Artificial Event Log. Eindhoven University of Technology. Dataset. https://doi.org/10.4121/uuid:32cad43f-8bb9-46af-8333-48aae2bea037
 #' @source <https://data.4tu.nl/repository/uuid:32cad43f-8bb9-46af-8333-48aae2bea037>
